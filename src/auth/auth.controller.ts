95d5ab6df4fb63ba10f27cb3c1e49c60ed167d4b
Sudeep Gautam <sudeepgautam777@gmail.com>
2025-10-07 13:51:47 +0545

    Adding files

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() authPayload: AuthPayloadDto) {
+    const user = this.authService.validateUser(authPayload);
+
+    if (!user) throw new HttpException('Invalid data', 401);
+
+    return user;
+  }
+}
